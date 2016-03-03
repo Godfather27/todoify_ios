4348304e38920564df9a4e329e5a946b848b15b8
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-03-03 13:44:54 +0100

    Initial Commit

---
+++ todoify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todoify
+//
+//  Created by Sebastian Huber on 03.03.16.
+//  Copyright © 2016 FH Salzburg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
