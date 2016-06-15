e49f1d19412a68cad63315079e71f56af7fdcd94
Pablo Pernias <pablo@pernias.com>
2016-06-15 09:41:17 +0200

    fixed text imprint

--- todoify/ImprintController.swift
+++ todoify/ImprintController.swift
@@ -24,7 +24,7 @@ class ImprintController: UIViewController, UIScrollViewDelegate {
         
         self.scrollView = UIScrollView()
         self.scrollView.delegate = self
-        self.scrollView.contentSize = CGSizeMake(self.view.frame.width, (3794 + 2*(imageSize + padding + textSize + labelPadding)))
+        self.scrollView.contentSize = CGSizeMake(self.view.frame.width, (300 + 2*(imageSize + padding + textSize + labelPadding)))
         
         contributorsView = setContriubtorsView()
         legalTextView = setLegalTextView()
@@ -93,55 +93,18 @@ class ImprintController: UIViewController, UIScrollViewDelegate {
         let label1 = UILabel(frame: CGRectMake(10, 0, (scrollView.contentSize.width-20), CGFloat.max))
         
         label1.numberOfLines = 0
-        
         label1.text = "Impressum\n" +
-        "Angaben gemäß § 5 TMG:\n" +
+            "Angaben gemäß § 5 TMG:\n" +
             "\n" +
             "Fachhochschule Salzburg\n" +
+            "MultiMediaTechnology - MMT\n" +
             "Campus Urstein Süd 1\n" +
             "A-5412 Puch/Salzburg\n" +
             "\n" +
             "Kontakt:\n" +
             "\n" +
             "Telefon: +43-(0)50-2211-0\n" +
-            "E-Mail: office@fh-salzburg.ac.at\n" +
-            " \n" +
-            "\n" +
-            "Quelle: eRecht24\n" +
-            "\n" +
-            "Haftungsausschluss (Disclaimer)\n" +
-            "Haftung für Inhalte\n" +
-            "\n" +
-            "Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.\n" +
-            "\n" +
-            "Haftung für Links\n" +
-            "\n" +
-            "Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.\n" +
-            "\n" +
-            "Urheberrecht\n" +
-            "\n" +
-            "Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.\n" +
-            "\n" +
-            "\n" +
-            "\n" +
-            "Datenschutzerklärung:\n" +
-            "Datenschutz\n" +
-            "\n" +
-            "Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.\n" +
-            "\n" +
-            "Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.\n" +
-            "\n" +
-            "Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.\n" +
-            "\n" +
-            " \n" +
-            "\n" +
-            "Datenschutzerklärung für die Nutzung von Google +1\n" +
-            "\n" +
-            "Unsere Seiten nutzen Funktionen von Google +1. Anbieter ist die Google Inc. 1600 Amphitheatre Parkway Mountain View, CA 94043,  USA.\n" +
-            "\n" +
-            "Erfassung und Weitergabe von Informationen: Mithilfe der Google +1-Schaltfläche können Sie Informationen weltweit veröffentlichen. über die Google +1-Schaltfläche erhalten Sie und andere Nutzer personalisierte Inhalte von Google und unseren Partnern. Google speichert sowohl die Information, dass Sie für einen Inhalt +1 gegeben haben, als auch Informationen über die Seite, die Sie beim Klicken auf +1 angesehen haben. Ihre +1 können als Hinweise zusammen mit Ihrem Profilnamen und Ihrem Foto in Google-Diensten, wie etwa in Suchergebnissen oder in Ihrem Google-Profil, oder an anderen Stellen auf Websites und Anzeigen im Internet eingeblendet werden. Google zeichnet Informationen über Ihre +1-Aktivitäten auf, um die Google-Dienste für Sie und andere zu verbessern. Um die Google +1-Schaltfläche verwenden zu können, benötigen Sie ein weltweit sichtbares, öffentliches Google-Profil, das zumindest den für das Profil gewählten Namen enthalten muss. Dieser Name wird in allen Google-Diensten verwendet. In manchen Fällen kann dieser Name auch einen anderen Namen ersetzen, den Sie beim Teilen von Inhalten über Ihr Google-Konto verwendet haben. Die Identität Ihres Google- Profils kann Nutzern angezeigt werden, die Ihre E-Mail-Adresse kennen oder über andere identifizierende Informationen von Ihnen verfügen.\n" +
-            "\n" +
-        "Verwendung der erfassten Informationen: Neben den oben erläuterten Verwendungszwecken werden die von Ihnen bereitgestellten Informationen gemäß den geltenden Google-Datenschutzbestimmungen genutzt. Google veröffentlicht möglicherweise zusammengefasste Statistiken über die +1-Aktivitäten der Nutzer bzw. gibt diese an Nutzer und Partner weiter, wie etwa Publisher, Inserenten oder verbundene Websites."
+        "E-Mail: office@fh-salzburg.ac.at\n"
         
         label1.sizeToFit()
         
